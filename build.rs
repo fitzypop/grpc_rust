e17883e67663248b7c25bc36592febb11fb81ff6
fitzypop <32967490+fitzypop@users.noreply.github.com>
2022-11-01 20:47:40 -0600

    update TodoService

--- build.rs
+++ build.rs
@@ -1,5 +1,3 @@
-use std::{env, path::PathBuf};
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
--- src/server.rs
+++ src/server.rs
@@ -7,31 +7,31 @@ mod todo {
     include!("todo.rs");
 }
 
-#[derive(Default, Debug)]
-struct TodoImpl {}
+#[derive(Debug, Default)]
+struct TodoService {}
 
 #[tonic::async_trait]
-impl Todo for TodoImpl {
+impl Todo for TodoService {
     async fn create_item(
         &self,
         request: Request<CreateTodoRequest>,
     ) -> Result<Response<TodoResponse>, Status> {
-        let todo_req = request.into_inner();
+        let _todo_req = request.into_inner();
         todo!()
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
-    let todo = TodoImpl::default();
+    let addr = "[::1]:50051".parse()?;
+    let todo_service = TodoService::default();
 
     let reflection_service = tonic_reflection::server::Builder::configure()
         .build()
         .unwrap();
 
     Server::builder()
-        .add_service(TodoServer::new(todo))
+        .add_service(TodoServer::new(todo_service))
         .add_service(reflection_service)
         .serve(addr)
         .await?;
